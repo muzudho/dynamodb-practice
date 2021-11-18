251344a43895a7b6bea79c35c33abef8ef843b5c
むずでょ <muzudho1@gmail.com>
2021-11-18 22:08:24 +0900

    Update e_gov_bestmove.py

--- e_gov_bestmove.py
+++ e_gov_bestmove.py
@@ -3,15 +3,37 @@ python.exe e_gov_bestmove.py
 """
 
 from pprint import pprint
-import boto3
-from botocore.exceptions import ClientError
 from e_gov_scan_bestmove_table import scan_bestmove_table
 
+def get_bestmove():
+    item_list = scan_bestmove_table()
+    if item_list:
+        print("Scan bestmove table succeeded:")
+        pprint(item_list, sort_dicts=False)
+
+        move_dict = {}
+
+        for item in item_list:
+            # move
+            m = item['bestmove']
+
+            if m in move_dict.keys():
+                move_dict[m] += 1
+            else:
+                move_dict[m] = 1
+
+    max_key = None
+    max_value = 0
+
+    for key, value in move_dict.items():
+        if max_value < value:
+            max_key = key
+            max_value = value
+
+    print(f"max_key=[{max_key}] max_value=[{max_value}]")
+    return max_key
 
 if __name__ == '__main__':
-    items = scan_bestmove_table()
-    if items:
-        print("Scan bestmove table succeeded:")
-        pprint(items, sort_dicts=False)
-
-        # items['bestmove']
+    # move
+    m = get_bestmove()
+    print(f"bestmove=[{m}]")
